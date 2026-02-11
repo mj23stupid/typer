702b3086a1d116fc854987b777ebb1b93d408dbb
trevato <me@trevato.dev>
2026-02-10 22:14:18 -0600

    flke

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Typing practice in your terminal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+      in
+      {
+        packages = {
+          default = python.pkgs.buildPythonApplication {
+            pname = "typer-cli-tool";
+            version = "0.1.0";
+            src = ./.;
+            format = "pyproject";
+
+            nativeBuildInputs = [ python.pkgs.setuptools ];
+
+            meta = {
+              description = "Typing practice in your terminal";
+              homepage = "https://github.com/William-Ger/typer";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "typer";
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            python.pkgs.setuptools
+            python.pkgs.pip
+          ];
+        };
+      }
+    );
+}
